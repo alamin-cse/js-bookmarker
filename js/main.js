201c6df31831eb123b295b60dc395c115303380e
Al Amin <aminal.cse@gmail.com>
2018-02-26 19:36:36 +0100

    added comments

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@
 
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
+// Save bookmark
 function saveBookmark(e) {
-  console.log('hi there');
+  //console.log('hi there');
 
+// Prevent form from submitting
   e.preventDefault();
 }
